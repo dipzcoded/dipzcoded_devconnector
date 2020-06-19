dc6cf38e1680bd9ae06e2f0fa4cbdf2c0ac0bc35
hp <ithub>
2020-06-19 04:21:26 +0100

    Prepared for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
+const path = require("path");
 //  connect to database
 connectDB();
 
@@ -14,8 +15,15 @@ app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 
-//  listen to a port
-app.get("/", (req, res) => res.send("API RUNNING"));
+//  serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.send(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server started on ${PORT}`);
